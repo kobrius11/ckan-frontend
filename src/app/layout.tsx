a66c94bae0a30bf6490cf5f85b5abb7823524d37
kobrius11 <kobrius11@gmail.com>
2025-05-29 20:37:01 +0300

    initial

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "@/app/styles/globals.css";
+import { geistMono, geistSans } from "@/app/fonts";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
